adbbbb3e91cd727891b2b152f7affb55d3ac4cae
andrew harrison <andrew.harrison@brandingbrand.com>
2015-03-20 12:35:43 -0400

    updates

---
+++ zsh/.zsh/aliases.zsh
@@ -0,0 +1,31 @@
+#                 ██      
+#                ░██      
+#  ██████  ██████░██      
+# ░░░░██  ██░░░░ ░██████  
+#    ██  ░░█████ ░██░░░██ 
+#   ██    ░░░░░██░██  ░██ 
+#  ██████ ██████ ░██  ░██ 
+# ░░░░░░ ░░░░░░  ░░   ░░  
+#
+#  ▓▓▓▓▓▓▓▓▓▓
+# ░▓ author ▓ xero <x@xero.nu>
+# ░▓ code   ▓ http://code.xero.nu/dotfiles
+# ░▓ mirror ▓ http://git.io/.files
+# ░▓▓▓▓▓▓▓▓▓▓
+# ░░░░░░░░░░
+#
+#█▓▒░ aliases
+alias ls="ls --color=auto"
+alias lsla="ls -la --color=auto"
+alias lsls="ls -la --color=auto"
+alias lsl="ls -l --color=auto"
+alias "cd.."="cd ../"
+alias up="cd ../"
+alias rm='rm -i'
+alias cp='cp -i'
+alias mv='mv -i'
+alias less='less -R'
+alias vi='vim'
+alias sprunge="curl -F 'sprunge=<-' http://sprunge.us"
+alias errlog="echo '' > /var/www/error.log && echo '' > /var/www/access.log"
+
---
+++ zsh/.zsh/settings.zsh
@@ -0,0 +1,29 @@
+#                 ██      
+#                ░██      
+#  ██████  ██████░██      
+# ░░░░██  ██░░░░ ░██████  
+#    ██  ░░█████ ░██░░░██ 
+#   ██    ░░░░░██░██  ░██ 
+#  ██████ ██████ ░██  ░██ 
+# ░░░░░░ ░░░░░░  ░░   ░░  
+#
+#  ▓▓▓▓▓▓▓▓▓▓
+# ░▓ author ▓ xero <x@xero.nu>
+# ░▓ code   ▓ http://code.xero.nu/dotfiles
+# ░▓ mirror ▓ http://git.io/.files
+# ░▓▓▓▓▓▓▓▓▓▓
+# ░░░░░░░░░░
+#
+#█▓▒░ timestamps
+#HIST_STAMPS="mm/dd/yyyy"
+
+#█▓▒░ paths
+export PATH=$HOME/bin:/usr/local/bin:/home/xero/.gem/ruby/2.2.0/bin:$PATH
+#export MANPATH="/usr/local/man:$MANPATH"
+
+#█▓▒░ preferred editor for local and remote sessions
+export EDITOR='vim'
+export VISUAL='vim'
+
+#█▓▒░ language
+export LC_CTYPE=en_US.utf8
