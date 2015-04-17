1c94a59b751ddd866b1770a6ab14ae30006aad31
andrew harrison <andrew.harrison@brandingbrand.com>
2015-04-17 14:41:22 -0400

    update zsh aliases

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -25,7 +25,9 @@ alias rm='rm -i'
 alias cp='cp -i'
 alias mv='mv -i'
 alias less='less -R'
+alias v='vim'
 alias vi='vim'
+alias tree='tree -CAFa -I "CVS|*.*.package|.svn|.git|.hg|node_modules|bower_components" --dirsfirst'
+alias ix="curl -F 'f:1=<-' ix.io"
 alias sprunge="curl -F 'sprunge=<-' http://sprunge.us"
 alias errlog="echo '' > /var/www/error.log && echo '' > /var/www/access.log"
-
